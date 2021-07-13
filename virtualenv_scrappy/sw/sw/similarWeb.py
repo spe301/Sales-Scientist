7935c3989fa3581f9b87842e267feb9914b39c26
Spencer Holley <aacjpw@gmail.com>
2021-07-13 06:48:40 -0500

    rantests on web crawler

--- virtualenv_scrappy/sw/sw/similarWeb.py
+++ virtualenv_scrappy/sw/sw/similarWeb.py
@@ -1,9 +1,10 @@
 import scrapy
 
-
 class PostsSpider(scrapy.Spider):
     name = 'posts'
-    start_urls = ['https://www.similarweb.com/website/millionaire.it/']
+    f = open(r'C:\Users\aacjp\Spencer\similarWeb.txt').read()
+    #start_urls = ['https://www.similarweb.com/website/webuildbrands.com/']
+    start_urls = f.split(' ')[:-1]
     
     '''def parse(self, response):
         page = response.url.split('/')[-1]
@@ -13,17 +14,37 @@ class PostsSpider(scrapy.Spider):
         return response.body'''
     
     def parse(self, response):
-        visits = float(response.css('span.engagementInfo-valueNumber.js-countValue::text').get().replace('K', ''))*1000
-        trajectory = float(response.css('span.websitePage-relativeChangeNumber::text').get().replace('%', ''))/100
-        deltaVisits = visits * trajectory
-        breakdown = response.css('div.trafficSourcesChart-values::text').getall()
-        #bounceRate = float(response.css('div.engagementInfo-line::text').get().replace('%', ''))
-        percentSearch = float(breakdown[2].replace('%', ''))/100
-        percentSocial = float(breakdown[3].replace('%', ''))/100
-        percentDisplay = float(breakdown[5].replace('%', ''))/100
-        percentSearchPaid = float(response.css('span.searchPie-number::text').getall()[1].replace('%', ''))/100
-        topPlatform = response.css('a.socialItem-title.name.link::text').get()
-        return visits, deltaVisits, percentSearch, percentSocial, percentDisplay, percentSearchPaid, topPlatform
-    
-    
+        overview = response.css('span.engagementInfo-valueNumber.js-countValue::text').getall()
+        if len(overview):
+            try:
+                visits = float(overview[0].replace('K', ''))*1000
+            except:
+                try:
+                    visits = (float(overview[0].replace('K', '').replace('<', ''))*1000)-1
+                except:
+                    visits = float(overview[0].replace('M', ''))*1000000
+            trajectory = float(response.css('span.websitePage-relativeChangeNumber::text').get().replace('%', ''))/100
+            deltaVisits = round(visits * trajectory)
+            breakdown = response.css('div.trafficSourcesChart-value::text').getall()
+            bounceRate = float(overview[-1].replace('%', ''))
+            percentSearch = float(breakdown[2].replace('%', ''))/100
+            percentSocial = float(breakdown[3].replace('%', ''))/100
+            percentDisplay = float(breakdown[5].replace('%', ''))/100
+            percentSearchPaid = float(response.css('span.searchPie-number::text').getall()[1].replace('%', ''))/100
+            topPlatform = response.css('a.socialItem-title.name.link::text').get()
+        else:
+            visits = 9999
+            deltaVisits = 0
+            bounceRate = None
+            percentSearch = None
+            percentSocial = None
+            percentDisplay = None
+            percentSearchPaid = None
+            topPlatform = None
+        percentPaidTraffic = (percentSocial+percentDisplay) + (percentSearchPaid*percentSearch)
+        yield {'% paid traffic': percentPaidTraffic, 
+               'visits': visits, 
+               'monthly visits change': deltaVisits, 
+               'bounce rate': bounceRate, 
+               'dominant platform': topPlatform}
         
