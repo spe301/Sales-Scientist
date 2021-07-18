b683e5705ea10854f33a1fdcb02e88f98e5457d9
spe301 <56374336+spe301@users.noreply.github.com>
2021-07-18 07:04:44 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,9 +2,7 @@ import pandas as pd
 import scrapy
 
 class individualFeatures:
-    '''def __init__(self, percentSocial, percentAds):
-        self.percentSocial = percentSocial
-        self.percentAds = percentAds'''
+    '''trigger words are words that are associated with a call to action. We are creating a list of them so that we can cout the number of trigger words a business has in their landing page. A lot of trigger words indicate multiple CTA's, this is a sign of spending too much on ads.'''
     
     def percentPaidSearch(self, percentSearch, percentPaid):
         return percentSearch * (percentPaid/100)
@@ -18,12 +16,32 @@ class individualFeatures:
     def percentAdTraffic(self, percentSocial, percentAds, percentPaidSearch):
         return round(percentSocial+percentAds+percentPaidSearch, 2)
     
-class aggregateFeatures:
-    triggerWords = ['book', 'call', 'today', 'get', 'my', 'free', 'copy', 'checklist', 
+    def nWords(self, landingPage):
+        words = 0
+        for lp in landingPage.split(' '):
+            if lp.startswith('http') == False:
+                words += 1
+        return words
+    
+    def triggerWords(self, landingPage):
+        triggerWords = ['book', 'call', 'today', 'get', 'my', 'free', 'copy', 'checklist', 
                 'pdf', 'report', 'ebook', 'join', 'session', 'now', 'set', 'email',
                 'free', 'list', 'attend', 'start', 'access', 'training', 'speak', 
                 'expert', 'here']
-    '''trigger words are words that are associated with a call to action. We are creating a list of them so that we can cout the number of trigger words a business has in their landing page. A lot of trigger words indicate multiple CTA's, this is a sign of spending too much on ads.'''
+        tw = 0
+        for lp in landingPage.split(' '):
+            if lp in triggerWords:
+                tw += 1
+        return tw
+    
+    def nLinks(self, landingPage):
+        links = 0
+        for lp in landingPage.split(' '):
+            if lp.startswith('http') == True:
+                links += 1
+        return links
+    
+class aggregateFeatures:
     
     def adRevenue(self, revenue, percentAdTraffic):
         return revenue * (percentAdTraffic/100)
